e66b28ec93df02ca971eafbc4c27d818af444cdd
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-05-08 14:11:22 -0400

    finished stretch

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -122,16 +122,55 @@ const archer = new Humanoid({
 
 console.log(mage.createdAt); // Today's date
 console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-console.log(swordsman.healthPoints); // 15
-console.log(mage.name); // Bruce
-console.log(swordsman.team); // The Round Table
-console.log(mage.weapons); // Staff of Shamalama
-console.log(archer.language); // Elvish
-console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-console.log(mage.takeDamage()); // Bruce took damage.
-console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log("\n" + swordsman.healthPoints + "\n"); // 15
+console.log(mage.name + "\n"); // Bruce
+console.log(swordsman.team + "\n"); // The Round Table
+console.log(mage.weapons + "\n"); // Staff of Shamalama
+console.log(archer.language + "\n"); // Elvish
+console.log(archer.greet() + "\n"); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage() + "\n"); // Bruce took damage.
+console.log(swordsman.destroy() + "\n" + "\n" + "\n" + "\n"); // Sir Mustachio was removed from the game.
 
 // Stretch task:
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+const Hero = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Hero",
+  team: "Good guy",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+const Villan = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 1000,
+  name: "Villan",
+  team: "bad guy",
+  weapons: ["Dagger"],
+  language: "common"
+});
+
+console.log(Hero.greet() + "\n");
+console.log(Villan.greet() + "\n");
+
+while (Hero.healthPoints > 0) {
+  console.log(Villan.takeDamage());
+  console.log(`Villan's health is ${(Villan.healthPoints -= 15)}\n`);
+  console.log(Hero.takeDamage());
+  console.log(`Hero's health is ${(Hero.healthPoints -= 1)}\n`);
+}
+console.log(Hero.destroy());
